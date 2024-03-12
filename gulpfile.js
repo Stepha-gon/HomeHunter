ffbdddb6907025bb632da2f80fd7ff75b072296e
Stepha Gonzalez <gstephaniegonz@gmail.com>
2024-03-12 16:23:28 -0500

    initial commit: installed dependencies, and created global variables and files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, watch } = require("gulp");
+//*CSS
+
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
+function css(done) {
+  //identificar archivo sass
+  //Compilarlo
+  //almacenarla en el disco duro
+  src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"));
+
+  done();
+}
+function dev(done) {
+  watch("src/scss/**/*.scss", css);
+  done();
+}
+exports.css = css;
+
+exports.dev = dev;
